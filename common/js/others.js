ccc31e042d6397d3fb5c2313ec26400c9925938f
kyms08 <bbxjd791@gmail.com>
2025-02-04 11:42:18 +0900

    header追加

---
+++ common/js/others.js
@@ -0,0 +1,6 @@
+'usestrict'
+
+document.getElementById('menuButton').addEventListener('click', function() {
+  const drawer = document.getElementById('drawerMenu');
+  drawer.classList.toggle('open');
+});
